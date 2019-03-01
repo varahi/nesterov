f0b42b42e12825e4581b93084c1b89e41c236ad5
Dmitry Vasilev <dmitry@t3dev.ru>
2019-03-01 19:19:31 +0300

    init extension tmpl

---
+++ public_html/typo3conf/ext/tmpl/ext_tables.php
@@ -0,0 +1,32 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+/**
+ * Page TS Config
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig
+('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tmpl/Configuration/TSConfig/page.ts">');
+
+/**
+ * add some basic User TS Config
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig
+('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tmpl/Configuration/TSConfig/user.ts">');
+
+
+/**
+ * Main Typoscript
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tmpl', 'Configuration/TypoScript', 'Site Template');
+
+
+/*
+call_user_func(
+    function()
+    {
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tmpl', 'Configuration/TypoScript', 'Site Template');
+
+    }
+);
+*/
